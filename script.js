be4e85eb770dfbd629bc92e089bd75c833e024c6
Rafael Silva <rafasilva320.rs@gmail.com>
2020-05-24 13:32:34 -0300

    create the snake and define size

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
 
-function createBackGround() {
+createBackGround = () => {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
-}
+};
+
+createSnake = () => {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = "red";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+};
 
 createBackGround();
+createSnake();
